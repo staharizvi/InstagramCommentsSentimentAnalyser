ed21149b967585cb4d51552afbaa5f78d851af62
Syed Taha Rizvi <161812427+taharizvi-ai@users.noreply.github.com>
2024-05-19 12:49:24 +0500

    Update instagramcomments.py
    added sentimental analysis to the scrapped comments

--- instagramcomments.py
+++ instagramcomments.py
@@ -1,27 +1,50 @@
 import instaloader
 import pandas as pd
+import nltk
+from textblob import TextBlob
+import spacy
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-user=[]
-coment=[]
-# Create an Instaloader instance
+spc = spacy.load("en_core_web_sm")
+
 loader = instaloader.Instaloader()
+loader.context.login('Username', 'Password') #<--- Replace Username & Password with your account
 
-# Log in to an Instagram account 
-loader.context.login('YOUR USERNAME', 'YOUR PASSWORD')
+url = 'https://www.instagram.com/p/XXXXXXXX/' #Replace with post URL which you want to analyze
+shortcode = instaloader.Post.from_shortcode(loader.context, url.split('/')[-2]).shortcode
 
-# Target post URL (replace 'post_url' with the URL of the post you want to scrape comments from)
-post_url = 'https://www.instagram.com/p/XXXXXXXXXXX/'
-shortcode = instaloader.Post.from_shortcode(loader.context, post_url.split('/')[-2]).shortcode
-
-# Get the post by its shortcode
 post = instaloader.Post.from_shortcode(loader.context, shortcode)
 
-# Get comments
 comments_list = [{'Username': comment.owner.username, 'Comment': comment.text} for comment in post.get_comments()]
 
-# Convert list of comments to DataFrame
 comments_df = pd.DataFrame(comments_list)
-comments_df.to_csv('comments.csv', index=False)
 
-# Logout
-loader.context.close()
+def analyze_entity_sentiment(comment):
+    doc = spc(comment)
+    entities_sentiments = []
+    for ent in doc.ents:
+        if ent.label_ in ['PERSON', 'ORG', 'GPE']:  # Consider only person, organization, and geopolitical entities
+            entity_sentiment = TextBlob(ent.text).sentiment.polarity
+            sentiment = 'Positive' if entity_sentiment > 0 else 'Negative' if entity_sentiment < 0 else 'Neutral'
+            entities_sentiments.append({'Entity': ent.text, 'Sentiment': sentiment})
+    return entities_sentiments
+
+comments_df['Entity_Sentiments'] = comments_df['Comment'].apply(analyze_entity_sentiment)
+
+all_comments = ' '.join(comments_df['Comment'])
+
+wordcloud = WordCloud(width = 800, height = 800, 
+                background_color ='white', 
+                stopwords = nltk.corpus.stopwords.words('english'), 
+                min_font_size = 10).generate(all_comments)
+                        
+plt.figure(figsize = (8, 8), facecolor = None) 
+plt.imshow(wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0) 
+plt.show()
+
+comments_df.to_csv('comments_with_entity_sentiment.csv', index=False)
+
+loader.context.logout()
